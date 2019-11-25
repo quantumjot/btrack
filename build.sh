1661a072fbf6d1144c020c949e793398a2c8aad1
quantumjot <code@arlowe.co.uk>
2019-11-25 18:54:06 +0000

    Added build script

---
+++ build.sh
@@ -0,0 +1,21 @@
+echo "Building btrack..."
+# make compilation directories
+mkdir ./btrack/libs
+mkdir ./btrack/obj
+
+# clone Eigen
+if [ -e ./btrack/include/eigen/signature_of_eigen3_matrix_library ]
+then
+  echo "Eigen already exists."
+else
+  git clone https://github.com/eigenteam/eigen-git-mirror.git ./btrack/include/eigen
+fi
+
+# build the tracker
+echo "Compiling btrack from source..."
+make clean
+make
+
+# run the installation
+echo "Installing btrack python package..."
+pip install .

268770dc7d83ae0a05b3c1093d5974e7bcf5c917
quantumjot <code@arlowe.co.uk>
2021-04-21 11:16:14 +0100

    update tests2

--- tests/test_tracklets.py
+++ tests/test_tracklets.py
@@ -1,66 +1,24 @@
 import numpy as np
 import pytest
-
-import btrack
-
-
-def _create_test_object():
-    """Create a test object."""
-    data = {
-        "ID": np.random.randint(0, 1000),
-        "x": np.random.uniform(0.0, 1000.0),
-        "y": np.random.uniform(0.0, 1000.0),
-        "z": np.random.uniform(0.0, 1000.0),
-        "t": np.random.randint(0, 1000),
-        "dummy": False,
-        "states": 5,
-        "label": 0,
-        "prob": 0.5,
-    }
-
-    obj = btrack.btypes.PyTrackObject().from_dict(data)
-    return obj, data
-
-
-def _create_test_properties():
-    properties = {
-        "speed": np.random.uniform(0.0, 1.0),
-        "circularity": np.random.uniform(0.0, 1.0),
-        "reporter": np.random.uniform(0.0, 1.0),
-    }
-    return properties
-
-
-def _create_test_tracklet(track_len: int):
-    """Create a test track."""
-    data = [_create_test_object()[0] for i in range(track_len)]
-    props = [_create_test_properties() for i in range(track_len)]
-    for idx, obj in enumerate(data):
-        obj.properties = props[idx]
-    track_ID = np.random.randint(0, 1000)
-    tracklet = btrack.btypes.Tracklet(track_ID, data)
-
-    # convert to dictionary {key: [p0,...,pn]}
-    if not props:
-        properties = {}
-    else:
-        properties = {k: [p[k] for p in props] for k in props[0].keys()}
-
-    return tracklet, data, properties, track_ID
+from _utils import (
+    create_test_object,
+    create_test_properties,
+    create_test_tracklet,
+)
 
 
 def test_object():
     """Test that an object is correctly instantiated, and that the stored
     data matches the data used for creation."""
-    obj, data = _create_test_object()
+    obj, data = create_test_object()
     for k, v in data.items():
         assert getattr(obj, k) == v
 
 
-@pytest.mark.parametrize("properties", [{}, _create_test_properties()])
+@pytest.mark.parametrize("properties", [{}, create_test_properties()])
 def test_object_properties(properties: dict):
     """Test an object with some properties."""
-    obj, data = _create_test_object()
+    obj, data = create_test_object()
     obj.properties = properties
     for k, v in properties.items():
         assert obj.properties[k] == v
@@ -70,7 +28,7 @@ def test_object_properties(properties: dict):
 def test_tracklet(track_len: int):
     """Test that a track is correctly instantiated, and that the stored
     data matches the data used for creation."""
-    tracklet, data, properties, track_ID = _create_test_tracklet(track_len)
+    tracklet, data, properties, track_ID = create_test_tracklet(track_len)
     assert len(tracklet) == len(data)
 
     # now check that the track data is correct
@@ -85,21 +43,8 @@ def test_tracklet(track_len: int):
 def test_tracklet_properties(track_len: int):
     """Test that a track is correctly instantiated, and that the stored
     properties match the data used for creation."""
-    tracklet, data, properties, track_ID = _create_test_tracklet(track_len)
+    tracklet, data, properties, track_ID = create_test_tracklet(track_len)
     t_properties = tracklet.properties
 
     for k, v in properties.items():
         np.testing.assert_equal(t_properties[k], v)
-
-
-# def test_malformed_tracklet_properties():
-#     """Test for malformed properties by truncating the first property."""
-#     track_len = 10
-#     tracklet, data, properties, track_ID = _create_test_tracklet(track_len)
-#     first_key = list(properties.keys())[0]
-#     # this removes a property from one of the objects
-#     del tracklet._data[0].properties[first_key]
-#
-#     # raises a key error when trying to retreive the properties
-#     with pytest.raises(KeyError):
-#         _ = tracklet.properties

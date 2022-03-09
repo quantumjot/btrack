aae67164057519970beb00e4df2b0df0aa050e13
alessandrofelder <a.felder@ucl.ac.uk>
2022-03-09 16:25:53 +0000

    manually remove obsolete decorator

--- napari_btrack/_dock_widget.py
+++ napari_btrack/_dock_widget.py
@@ -6,7 +6,6 @@ see: https://napari.org/docs/dev/plugins/hook_specifications.html
 
 Replace code below according to your needs.
 """
-from napari_plugin_engine import napari_hook_implementation
 from qtpy.QtWidgets import QWidget, QHBoxLayout, QPushButton
 from magicgui import magic_factory
 
@@ -35,7 +34,6 @@ def example_magic_widget(img_layer: "napari.layers.Image"):
     print(f"you have selected {img_layer}")
 
 
-@napari_hook_implementation
 def napari_experimental_provide_dock_widget():
     # you can return either a single widget, or a sequence of widgets
     return [ExampleQWidget, example_magic_widget]

f9614033837699d135494cf6850cf9daf091b17a
David Stansby <dstansby@gmail.com>
2022-04-21 12:04:50 +0100

    Refactor HDF5 file creation to a fixture

---
+++ tests/conftest.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+import pytest
+
+import btrack.dataio
+from ._utils import (
+    create_test_object,
+    create_test_properties,
+    simple_tracker_example,
+)
+
+
+@pytest.fixture
+def test_objects():
+    """
+    Create a list of 10 test objects.
+    """
+    return [create_test_object(id=i)[0] for i in range(10)]
+
+
+@pytest.fixture
+def hdf5_file_path(tmp_path, test_objects) -> Path:
+    """
+    Create and save a btrack HDF5 file, and return the path.
+    """
+    fn = Path(tmp_path) / "test.h5"
+
+    with btrack.dataio.HDF5FileHandler(fn, "w") as h:
+        h.write_objects(test_objects)
+
+    return fn
--- tests/test_tracker.py
+++ tests/test_tracker.py
@@ -1,7 +1,7 @@
 import json
 
 import numpy as np
-from _utils import full_tracker_example, simple_tracker_example
+from ._utils import full_tracker_example, simple_tracker_example
 
 import btrack
 

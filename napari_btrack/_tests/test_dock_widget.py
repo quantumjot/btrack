272b546b87c834b22cdfc421187b48a0d3ff1644
alessandrofelder <a.felder@ucl.ac.uk>
2022-06-09 12:44:39 +0100

    write widget-config roundtrip test

--- napari_btrack/_tests/test_dock_widget.py
+++ napari_btrack/_tests/test_dock_widget.py
@@ -1,4 +1,8 @@
-from ..track import track
+import pytest
+from btrack.config import load_config
+from btrack.datasets import cell_config, particle_config
+
+from ..track import _tracker_config_to_widgets, _widgets_to_tracker_config, track
 
 
 def test_add_widget(make_napari_viewer):
@@ -6,3 +10,13 @@ def test_add_widget(make_napari_viewer):
     num_dw = len(list(viewer.window._dock_widgets))
     viewer.window.add_function_widget(function=track)
     assert len(list(viewer.window._dock_widgets)) == num_dw + 1
+
+
+@pytest.mark.parametrize("config", [cell_config(), particle_config()])
+def test_config_to_widgets_round_trip(config):
+    container_widget = track()
+    expected_config = load_config(config)
+    _tracker_config_to_widgets(container_widget, expected_config)
+    actual_config = _widgets_to_tracker_config(container_widget)
+
+    assert actual_config.json() == expected_config.json()

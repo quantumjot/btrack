d9ec8ce9baf49a4e7a6bd1825c60f47261cce8fe
alessandrofelder <a.felder@ucl.ac.uk>
2022-06-10 17:01:53 +0100

    document tests

--- napari_btrack/_tests/test_dock_widget.py
+++ napari_btrack/_tests/test_dock_widget.py
@@ -11,6 +11,7 @@ from ..track import _tracker_config_to_widgets, _widgets_to_tracker_config, trac
 
 
 def test_add_widget(make_napari_viewer):
+    """Checks that the track widget can be added inside a dock widget."""
     viewer = make_napari_viewer()
     num_dw = len(list(viewer.window._dock_widgets))
     viewer.window.add_function_widget(function=track)
@@ -19,16 +20,15 @@ def test_add_widget(make_napari_viewer):
 
 @pytest.fixture
 def track_widget():
+    """Provides an instance of the track widget to test"""
     return track()
-
-
-@pytest.fixture
-def user_config_path():
-    return "user_config.json"
 
 
 @pytest.mark.parametrize("config", [cell_config(), particle_config()])
 def test_config_to_widgets_round_trip(track_widget, config):
+    """Tests that going back and forth between
+    config objects and widgets works as expected.
+    """
     expected_config = load_config(config)
     _tracker_config_to_widgets(track_widget, expected_config)
     actual_config = _widgets_to_tracker_config(track_widget)
@@ -36,7 +36,39 @@ def test_config_to_widgets_round_trip(track_widget, config):
     assert actual_config.json() == expected_config.json()
 
 
+@pytest.fixture
+def user_config_path():
+    """Provides a (dummy) string to represent a user-provided config path."""
+    return "user_config.json"
+
+
+def test_save_button(user_config_path, track_widget):
+    """Tests that clicking the save configuration button
+    triggers a call to btrack.config.save_config with expected arguments.
+    """
+    with patch("napari_btrack.track.save_config") as save_config:
+        with patch("napari_btrack.track.get_save_path") as get_save_path:
+            get_save_path.return_value = user_config_path
+            track_widget.save_config_button.clicked()
+    assert save_config.call_args.args[0] == user_config_path
+    assert save_config.call_args.args[1].json() == load_config(cell_config()).json()
+
+
+def test_load_button(user_config_path, track_widget):
+    """Tests that clicking the load configuration button
+    triggers a call to btrack.config.load_config with the expected argument
+    """
+    with patch("napari_btrack.track.load_config") as load_config:
+        with patch("napari_btrack.track.get_load_path") as get_load_path:
+            get_load_path.return_value = user_config_path
+            track_widget.load_config_button.clicked()
+    assert load_config.call_args.args[0] == user_config_path
+
+
 def test_reset_button(track_widget):
+    """Tests that clicking the reset button with
+    particle-config-populated widgets resets to the default (i.e. cell-config)
+    """
     # change config to particle
     _tracker_config_to_widgets(track_widget, load_config(particle_config()))
 
@@ -47,27 +79,14 @@ def test_reset_button(track_widget):
     assert config_after_reset.json() == load_config(cell_config()).json()
 
 
-def test_save_button(user_config_path, track_widget):
-    with patch("napari_btrack.track.save_config") as save_config:
-        with patch("napari_btrack.track.get_save_path") as get_save_path:
-            get_save_path.return_value = user_config_path
-            track_widget.save_config_button.clicked()
-    assert save_config.call_args.args[0] == user_config_path
-    assert save_config.call_args.args[1].json() == load_config(cell_config()).json()
-
-
-def test_load_button(user_config_path, track_widget):
-    with patch("napari_btrack.track.load_config") as load_config:
-        with patch("napari_btrack.track.get_load_path") as get_load_path:
-            get_load_path.return_value = user_config_path
-            track_widget.load_config_button.clicked()
-    assert load_config.call_args.args[0] == user_config_path
-
-
 @pytest.fixture
 def simplistic_tracker_outputs():
-    N = 10
-    D = 3
+    """Provides simplistic return values of a btrack run.
+
+    They have the correct types and dimensions, but contain zeros or nothing.
+    Useful for mocking the tracker.
+    """
+    N, D = 10, 3
     data = np.zeros((N, D + 1))
     properties = dict(some_property=np.zeros((N)))
     graph = dict()
@@ -75,11 +94,15 @@ def simplistic_tracker_outputs():
 
 
 def test_run_button(make_napari_viewer, track_widget, simplistic_tracker_outputs):
+    """Tests that clicking the run button calls run_tracker,
+    and that the napari viewer has an additional tracks layer after running.
+    """
     with patch("napari_btrack.track.run_tracker") as run_tracker:
         run_tracker.return_value = simplistic_tracker_outputs
         viewer = make_napari_viewer()
         segmentation = datasets.example_segmentation()
         viewer.add_labels(segmentation)
+        # somehow the choices are not updated automatically here.
         track_widget.segmentation.choices = napari.current_viewer().layers
         track_widget.segmentation.value = napari.current_viewer().layers["segmentation"]
         track_widget.call_button.clicked()

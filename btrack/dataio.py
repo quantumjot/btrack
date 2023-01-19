4b57eaee8bbf3803e34ce3425633c6d730a35762
quantumjot <code@arlowe.co.uk>
2023-01-19 16:36:03 +0000

    fix fails

--- btrack/dataio.py
+++ btrack/dataio.py
@@ -2,12 +2,10 @@ import warnings
 
 from .io import HDF5FileHandler  # noqa: F401
 
-
 raise warnings.warn(
     "`btrack.dataio` has been deprecated. Please us `btrack.io` subpackage instead.",
     DeprecationWarning,
 )
-
 
 if __name__ == "__main__":
     pass

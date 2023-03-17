331728e73328011c4ef48e0c41fe538035f1c2c2
Paddy Roddy <patrickjamesroddy@gmail.com>
2023-03-17 16:52:02 +0000

    Hardcode the path :cry:

--- btrack/libwrapper.py
+++ btrack/libwrapper.py
@@ -78,10 +78,10 @@ def load_library(filename):
     system = platform.system()
 
     if system == "Windows":
-        os.add_dll_directory(os.path.join(BTRACK_PATH, "libs"))
-
-    file_ext = {"Linux": ".so", "Darwin": ".dylib", "Windows": ".DLL"}
-    full_lib_file = lib_file + file_ext[system]
+        full_lib_file = "D:\\a\\btrack\\btrack\\btrack\\libs\\libtracker.DLL"
+    else:
+        file_ext = {"Linux": ".so", "Darwin": ".dylib"}
+        full_lib_file = lib_file + file_ext[system]
 
     try:
         lib = ctypes.cdll.LoadLibrary(full_lib_file)

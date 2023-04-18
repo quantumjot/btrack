eb20515d931e42d13715d4efea0d9a839fa4efe5
quantumjot <code@arlowe.co.uk>
2023-04-18 14:26:41 +0100

    fix ruff complaints

--- btrack/io/__init__.py
+++ btrack/io/__init__.py
@@ -1,13 +1,12 @@
+from ._localization import segmentation_to_objects
 from .exporters import export_CSV, export_delegator, export_LBEP
 from .hdf import HDF5FileHandler
 from .importers import import_CSV
-from ._localization import segmentation_to_objects
 from .utils import (
     localizations_to_objects,
     objects_from_array,
     objects_from_dict,
 )
-
 
 __all__ = [
     "export_CSV",

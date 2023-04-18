eefa5296238d50bf35fa8434efdfd2c8fb3fd206
Alan R Lowe <code@arlowe.co.uk>
2023-04-18 14:23:09 +0100

    add import

--- btrack/io/__init__.py
+++ btrack/io/__init__.py
@@ -1,11 +1,13 @@
 from .exporters import export_CSV, export_delegator, export_LBEP
 from .hdf import HDF5FileHandler
 from .importers import import_CSV
+from ._localization import segmentation_to_objects
 from .utils import (
     localizations_to_objects,
     objects_from_array,
     objects_from_dict,
 )
+
 
 __all__ = [
     "export_CSV",

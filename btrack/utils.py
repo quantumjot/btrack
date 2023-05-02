11d2c8ed5c33768311bc409caa661bb460e27add
nthndy <day.nathan@pm.me>
2023-05-02 15:22:17 +0100

    scale option in `update_segmentation`, needs work for more dimensions

--- btrack/utils.py
+++ btrack/utils.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import dataclasses
 import functools
 import logging
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 import numpy as np
 from skimage.util import map_array
@@ -232,6 +232,7 @@ def update_segmentation(
     segmentation: np.ndarray,
     tracks: list[btypes.Tracklet],
     *,
+    scale: Optional[Tuple[float]] = None,
     color_by: str = "ID",
 ) -> np.ndarray:
     """Map tracks back into a masked array.
@@ -243,6 +244,9 @@ def update_segmentation(
         ordered T(Z)YX. Assumes that this is not binary and each object has a unique ID.
     tracks : list[btypes.Tracklet]
         A list of :py:class:`btrack.btypes.Tracklet` objects from BayesianTracker.
+    scale : tuple, optional
+        A scale for each spatial dimension of the input tracks. Defaults
+        to one for all axes, and allows scaling for anisotropic imaging data.
     color_by : str, default = "ID"
         A value to recolor the segmentation by.
 
@@ -274,6 +278,8 @@ def update_segmentation(
         ]
     )
 
+    scale = tuple([1.0] * segmentation.ndim) if scale is None else scale
+
     if color_by not in keys:
         raise ValueError(f"Property ``{color_by}`` not found in track.")
 
@@ -283,6 +289,9 @@ def update_segmentation(
 
         xc, yc = frame_coords[:, keys["x"]], frame_coords[:, keys["y"]]
         new_id = frame_coords[:, keys[color_by]]
+
+        xc = (xc * scale[0]).astype(int)
+        yc = (yc * scale[1]).astype(int)
 
         if single_segmentation.ndim == constants.Dimensionality.TWO:
             old_id = single_segmentation[yc, xc]
